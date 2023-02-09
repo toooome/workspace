e076c7455142eb0b513bd2ec15b8217ac2eab2a2
DESKTOP-IJ8C807\user1 <kim7s7722@naver.com>
2023-02-09 17:42:52 +0900

    230209 CP 과제

---
+++ 01_Java/a003_control/src/edu/kh/control/condition/practice/ConditionPractice.java
@@ -0,0 +1,53 @@
+package edu.kh.control.condition.practice;
+
+import java.util.Scanner;
+
+
+
+// 실습문제 작성 클래스
+public class ConditionPractice {
+	
+	public void practice1( ) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("숫자를 한 개 입력하세요 : ");
+		int num = sc.nextInt();
+			
+		if(num<0) {
+			System.out.println("양수만 입력해주세요");
+			
+		} else if (num/2 == 0) {
+			System.out.println("짝수입니다.");
+			
+		} else (num/2 == 1) {
+			System.out.println("홀수입니다.");
+		}
+		
+		
+	}
+	
+	
+	
+	public void practice2( ) {
+		
+	}
+	
+	
+	
+	public void practice3( ) {
+		
+	}
+	
+	
+	
+	public void practice4( ) {
+		
+	}
+	
+	
+	
+	public void practice5( ) {
+		
+	}
+}
