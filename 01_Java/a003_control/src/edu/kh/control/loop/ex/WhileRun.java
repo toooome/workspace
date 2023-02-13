7c0ba3c7e805eeb95f8893750162f547851e98e5
DESKTOP-IJ8C807\user1 <kim7s7722@naver.com>
2023-02-13 17:25:53 +0900

    230213 While

---
+++ 01_Java/a003_control/src/edu/kh/control/loop/ex/WhileRun.java
@@ -0,0 +1,14 @@
+package edu.kh.control.loop.ex;
+
+public class WhileRun {
+	public static void main(String[] args) {
+		
+		WhileEx we = new WhileEx();
+		
+//		we.ex1();
+//		we.ex2();		
+//		we.ex3();
+		we.ex4();
+		
+	}
+}
